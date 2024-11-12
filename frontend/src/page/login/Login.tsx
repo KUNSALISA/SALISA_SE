8ee1197d9af13f015668b597358d71bd6da27982
KUNSALISA <attawerapathsalisa@gmail.com>
2024-11-12 22:17:58 +0700

    page_Login

--- frontend/src/page/login/Login.tsx
+++ frontend/src/page/login/Login.tsx
@@ -1,35 +1,54 @@
 import React from 'react';
-import { Button, Input, Typography } from 'antd';
+import { Button, Input, Form, Typography } from 'antd';
 import './Login.css';
+import wareh from '../../assets/warehouse.jpg'
+import logocat from '../../assets/Logocat.png'
+import people_m from '../../assets/peoplemanage.png'
 
-const { Title, Text } = Typography;
+const { Title, Text, Link } = Typography;
 
 const WarehouseLogin: React.FC = () => {
   return (
     <div className="login-container">
-      <div className="background-image" />
-      <div className="form-container">
-        <div className="logo">
-          <img src="your-logo-url" alt="Category Box Logo" />
-        </div>
-        <Title className="welcome-title">
-          <span>W</span>elcome to the <span>WAREHOUSE</span>
-        </Title>
-        
-        <div className="login-form">
-          <Text className="label">Email</Text>
-          <Input className="input-field" type="email" placeholder="Enter your email" />
-          
-          <Text className="label">Password</Text>
-          <Input.Password className="input-field" placeholder="Enter your password" />
-          
-          <Text className="forgot-password">Forgot password?</Text>
-          
-          <Button type="primary" className="login-button">LOGIN</Button>
-          
-          <div className="signup-text">
-            Don’t have an account? <span className="signup-link">Sign Up</span>
+      <div className="background-square"></div>
+      <div className="background-square-1"></div>
+      <div className="background-square-2"></div>
+      <div className="background-square-3"></div>
+      <div className="background-square-4"></div>
+      <div className="background-square-5"></div>
+      <div className="background-square-6"></div>
+      <div className="warehouse-image">
+          <img src={wareh} alt="Warehouse" />
+      </div>
+      <div className="login-box">
+          <div className="employ-image">
+              <img src={people_m} alt="employ" />
           </div>
+        <div className="login-content">
+          <img src={logocat} alt="Logo" className="logo" />
+          <Title level={1} className="welcome-text">
+            <div className="text-wrapper">
+              <span className="highlight-text-1">W</span>
+              <div className="sub-text">
+                <span className="highlight-text-2"><span className="spaced-letter"></span>elcome to the</span>
+                <span className="highlight-text-3">AREHOUSE</span>
+              </div>
+            </div>
+          </Title>
+          <Form className="login-form" layout="vertical">
+            <Form.Item label="Email" name="email">
+              <Input placeholder="Enter your email" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input.Password placeholder="Enter your password" />
+              <Button type="primary" htmlType="submit" className="login-button">
+                LOGIN
+              </Button>
+            </Form.Item>
+            <Text>
+              Don't have an account? <Link>Sign Up</Link>
+            </Text>
+          </Form>
         </div>
       </div>
     </div>
