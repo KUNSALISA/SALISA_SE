8056cd2bd82c605f1e479ce0c127bbd86b6438b5
KUNSALISA <attawerapathsalisa@gmail.com>
2024-11-15 02:41:27 +0700

    main

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	_ "net/http"
+
+	_ "github.com/KUNSALISA/SALISA_SE/config"
+	_ "github.com/KUNSALISA/SALISA_SE/controller"
+	"github.com/gin-gonic/gin"
+)
+
+const PORT = "8080"
+
+func main() {
+
+	// // open connection database
+	// config.ConnectionDB()
+
+	// // Generate databases
+	// config.SetupDatabase()
+
+	// r := gin.Default()
+
+	// r.Use(CORSMiddleware())
+
+	// router := r.Group("")
+	// {
+
+	// 	// User Routes
+	// 	router.GET("/users", controller.ListUsers)
+	// 	router.GET("/user/:id", controller.GetUser)
+	// 	router.POST("/users", controller.CreateUser)
+	// 	router.PATCH("/users", controller.UpdateUser)
+	// 	router.DELETE("/users/:id", controller.DeleteUser)
+	// 	// Gender Routes
+	// 	router.GET("/genders", controller.ListGenders)
+	// }
+
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.String(http.StatusOK, "API RUNNING... PORT: %s", PORT)
+	// })
+
+	// // Run the server
+
+	// r.Run("localhost:" + PORT)
+
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
