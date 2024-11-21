62ff9cf30f28cf41bc106610411778e6504a3b19
KUNSALISA <attawerapathsalisa@gmail.com>
2024-11-21 15:54:03 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LoginPage from "../src/page/login/Login";
-// import Dashboard from "./pages/Dashboard";
-// import ManagerPage from "../src/page/dash/Dash";
-// import PageA from "./pages/PageA";
-// import PageB from "./pages/PageB";
-// import PageC from "./pages/PageC";
-// import PageD from "./pages/PageD";
-
-// import EmployeePage from "./pages/EmployeePage";
-// import CustomerPage from "./pages/CustomerPage";
-
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* เส้นทางเข้าสู่ระบบ */}
-//         <Route path="/" element={<LoginPage />} />
-        
-//         {/* เส้นทางสำหรับ Dashboard และผู้จัดการ */}
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/manager" element={<ManagerPage />} />
-        
-//         {/* เส้นทางสำหรับแต่ละหน้า */}
-//         <Route path="/page-a" element={<PageA />} />
-//         <Route path="/page-b" element={<PageB />} />
-//         <Route path="/page-c" element={<PageC />} />
-//         <Route path="/page-d" element={<PageD />} />
-        
-//         {/* เส้นทางใหม่: Warehouse */}
-
-//         <Route path="/employee" element={<EmployeePage />} />
-//         <Route path="/customer" element={<CustomerPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SharedLayout from "../src/components/Layout";
@@ -90,12 +49,13 @@ export default App;
 
 // import React from 'react';
 // import Login from '../src/page/login/Login';
-// import Employee from './page/employee/employee';
+// import ManagerPage from "../src/page/dash/Dash";
+// import SharedLayout from "../src/components/Layout";
 
 // const App: React.FC = () => {
 //   return (
 //     <div>
-//       <Login />
+//       <SharedLayout />
 //     </div>
 //   );
 // };
@@ -116,6 +76,48 @@ export default App;
 //         <Route path="/" element={<Home />} />
 //         <Route path="/employee" element={<Employee />} />
 //         <Route path="/customer" element={<Customer />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import LoginPage from "../src/page/login/Login";
+// import Dashboard from "./pages/Dashboard";
+// import ManagerPage from "../src/page/dash/Dash";
+// import PageA from "./pages/PageA";
+// import PageB from "./pages/PageB";
+// import PageC from "./pages/PageC";
+// import PageD from "./pages/PageD";
+
+// import EmployeePage from "./pages/EmployeePage";
+// import CustomerPage from "./pages/CustomerPage";
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* เส้นทางเข้าสู่ระบบ */}
+//         <Route path="/" element={<LoginPage />} />
+        
+//         {/* เส้นทางสำหรับ Dashboard และผู้จัดการ */}
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/manager" element={<ManagerPage />} />
+        
+//         {/* เส้นทางสำหรับแต่ละหน้า */}
+//         <Route path="/page-a" element={<PageA />} />
+//         <Route path="/page-b" element={<PageB />} />
+//         <Route path="/page-c" element={<PageC />} />
+//         <Route path="/page-d" element={<PageD />} />
+        
+//         {/* เส้นทางใหม่: Warehouse */}
+
+//         <Route path="/employee" element={<EmployeePage />} />
+//         <Route path="/customer" element={<CustomerPage />} />
 //       </Routes>
 //     </Router>
 //   );
