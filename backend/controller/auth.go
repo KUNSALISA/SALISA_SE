e6a6a292ff3c617c31dd7881e63b0abb1ed1588b
KUNSALISA <attawerapathsalisa@gmail.com>
2024-11-24 19:46:37 +0700

    _

--- backend/controller/auth.go
+++ backend/controller/auth.go
@@ -135,8 +135,8 @@ func SignInEmployees(c *gin.Context) {
 		"token_type":   "Bearer",
 		"token":        signedToken,
 		"id":           user.ID,
-		"access_level": user.AccessLevel, // ส่งค่า AccessLevel หรือ Position
-		"avatar":       user.Avatar, // ส่ง Avatar ไปด้วย
+		"access_level": user.AccessLevel, 
+		"avatar":       user.Avatar, 
 		"e_firstname":       user.E_FirstName,
 		"e_lastname":       user.E_LastName,
 	})
